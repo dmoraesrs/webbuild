0c0c8008d7b398cccee8ee14d99e77fb5baacd2d
Diego Moraes <diego.moraes@neogrid.com>
2020-09-19 06:19:06 -0300

    Primeito commit

---
+++ precache-manifest.c8bd96d08cf5d163431bdf6b5f433c91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6142a552e287ad78b590a1bcc32603",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c708f96df405d5e4c805",
+    "url": "/static/css/main.ab43a65d.chunk.css"
+  },
+  {
+    "revision": "0bb69f49e24df5052924",
+    "url": "/static/js/2.4e33ffdf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4e33ffdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c708f96df405d5e4c805",
+    "url": "/static/js/main.b17e9548.chunk.js"
+  },
+  {
+    "revision": "faed2ce97fd0189e8388",
+    "url": "/static/js/runtime-main.0e10c2e8.js"
+  }
+]);
